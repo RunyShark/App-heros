474ec16d23675f54b51491cc09141da3211cbf50
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-06 23:20:37 -0500

    navigate

--- src/heros/components/HeroID.jsx
+++ src/heros/components/HeroID.jsx
@@ -1,26 +1,55 @@
-import { useParams, Navigate } from "react-router";
+import { useParams, Navigate, useNavigate } from "react-router";
 import { getHeroById } from "../";
 
 export const HeroID = () => {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const hero = getHeroById(id);
-
-  if (!hero) {
-    return <Navigate to="/" />;
-  }
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   const { superhero, publisher, alter_ego, first_appearance, characters } =
     hero;
   const pathImgByHero = `../../../assets/heroes/${id}.jpg`;
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
   return (
-    <>
-      <h2>{superhero}</h2>
-      <h3>Editorial: {publisher}</h3>
-      <img src={pathImgByHero} alt={superhero} />
-      <h4>person: {alter_ego}</h4>
-      <p>Actores que han intnerpretado al super heroe: {characters}</p>
-      <p>Primera apracion: {first_appearance}</p>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={pathImgByHero} alt={superhero} className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <p>
+              <b>Editorial: </b>
+              {publisher}
+            </p>
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <p>
+              {" "}
+              <b>person: </b> {alter_ego}
+            </p>
+          </li>
+          <li className="list-group-item">
+            <p>
+              <b>Primera apracion: </b> {first_appearance}
+            </p>
+          </li>
+        </ul>
+        <h5 className="mt-3">
+          {characters.split(",").length > 1 ? "Actores" : "Actor"}
+        </h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
   );
 };
